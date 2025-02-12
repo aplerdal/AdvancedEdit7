aaece1194683fcce8ce22ee3a9099e192fcaaf08
Andrew Lerdal <aplerdal01@gmail.com>
2025-02-12 14:52:10 -0600

    Keyboard Controls. Fixes #4

--- AdvancedEdit/UI/Tools/View.cs
+++ AdvancedEdit/UI/Tools/View.cs
@@ -1,3 +1,4 @@
+using System;
 using AdvancedEdit.UI.Windows;
 using ImGuiNET;
 using Microsoft.Xna.Framework.Input;
@@ -8,6 +9,7 @@ namespace AdvancedEdit.UI.Tools;
 public class View : MapTool
 {
     const float ZoomFactor = 1.25f;
+    private float _panSpeed = 5f;
     private bool _dragging;
     private Vector2 _dragPosition = Vector2.Zero;
     private Vector2 _dragMapPosition = Vector2.Zero;
@@ -36,8 +38,8 @@ public class View : MapTool
                 else
                     window.Scale /= ZoomFactor;
                 var trackSize = new Vector2(window.Track.Size.X, window.Track.Size.Y);
-                Vector2 newTrackSize = trackSize * 8f * window.Scale;
-                window.Translation += relativeMousePosition - relativeMousePosition*(newTrackSize/window.MapSize);
+                Vector2 pixelTrackSize = trackSize * 8f * window.Scale;
+                window.Translation += relativeMousePosition - relativeMousePosition*(pixelTrackSize/window.MapSize);
             }
             
         }
@@ -50,5 +52,50 @@ public class View : MapTool
         {
             _dragging = false;
         }
+
+        #region Kb input
+
+        if (ImGui.IsKeyChordPressed(ImGuiKey.ModCtrl | ImGuiKey.Equal))
+        {
+            Vector2 center = ImGui.GetWindowPos()+ImGui.GetWindowSize()/2 - window.MapPosition;
+            window.Scale *= ZoomFactor;
+            var trackSize = new Vector2(window.Track.Size.X, window.Track.Size.Y);
+            Vector2 pixelTrackSize = trackSize * 8f * window.Scale;
+            window.Translation += center - center * (pixelTrackSize / window.MapSize);
+        }
+
+        if (ImGui.IsKeyChordPressed(ImGuiKey.ModCtrl | ImGuiKey.Minus))
+        {
+            Vector2 center = ImGui.GetWindowPos() + ImGui.GetWindowSize() / 2 - window.MapPosition;
+            window.Scale /= ZoomFactor;
+            var trackSize = new Vector2(window.Track.Size.X, window.Track.Size.Y);
+            Vector2 pixelTrackSize = trackSize * 8f * window.Scale;
+            window.Translation += center - center * (pixelTrackSize / window.MapSize);
+        }
+
+        // Does not work for me? maybe on windows build though.
+        _panSpeed = Math.Clamp((ImGui.IsKeyDown(ImGuiKey.ModShift) ? 10f : 5f) / window.Scale, 2.5f, 20f);
+        
+        if (ImGui.IsKeyDown(ImGuiKey.UpArrow))
+        {
+            window.Translation += new Vector2(0, _panSpeed);
+        }
+
+        if (ImGui.IsKeyDown(ImGuiKey.DownArrow))
+        {
+            window.Translation += new Vector2(0, -_panSpeed);
+        }
+
+        if (ImGui.IsKeyDown(ImGuiKey.LeftArrow))
+        {
+            window.Translation += new Vector2(_panSpeed, 0);
+        }
+
+        if (ImGui.IsKeyDown(ImGuiKey.RightArrow))
+        {
+            window.Translation += new Vector2(-_panSpeed, 0);
+        }
+        
+        #endregion
     }
 }
